987232040df57d6eea185b659841718cb041321c
Nathan Li <moncskoli@gmail.com>
2024-04-26 13:14:22 -0400

    update translate style

--- my-react-app/src/components/LanguageSwitcher.js
+++ my-react-app/src/components/LanguageSwitcher.js
@@ -1,61 +1,59 @@
-import React from 'react';
-import { Select, MenuItem, FormControl, InputLabel, IconButton } from '@mui/material';
-import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
+import React from "react";
+import { MenuItem, Box, TextField, IconButton } from "@mui/material";
+import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
 
 export default function LanguageSwitcher(props) {
-    const { languageFrom, setLanguageFrom, languageTo, setLanguageTo } = props;
+  const { languageFrom, setLanguageFrom, languageTo, setLanguageTo } = props;
 
-    const handleLanguageFromChange = (event) => {
-        let language = languageFrom;
-        setLanguageFrom(event.target.value);
-        setLanguageTo(language);
-    };
+  const handleLanguageFromChange = (event) => {
+    let language = languageFrom;
+    setLanguageFrom(event.target.value);
+    setLanguageTo(language);
+  };
 
-    const handleLanguageToChange = (event) => {
-        let language = languageTo;
-        setLanguageFrom(language);
-        setLanguageTo(event.target.value);
-    };
+  const handleLanguageToChange = (event) => {
+    let language = languageTo;
+    setLanguageFrom(language);
+    setLanguageTo(event.target.value);
+  };
 
-    const handleSwap = () => {
-        let language = languageFrom;
-        setLanguageFrom(languageTo);
-        setLanguageTo(language);
-    }
+  const handleSwap = () => {
+    let language = languageFrom;
+    setLanguageFrom(languageTo);
+    setLanguageTo(language);
+  };
 
-    return (
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <FormControl variant="filled" style={{ minWidth: 120, margin: 10 }}>
-                <InputLabel id="select-language-from-label">Translate from</InputLabel>
-                <Select
-                    labelId="select-language-from-label"
-                    id="select-language-from"
-                    value={languageFrom}
-                    onChange={handleLanguageFromChange}
-                    size="small"
-                >
-                    <MenuItem value="Text">Text</MenuItem>
-                    <MenuItem value="Emojis">Emojis</MenuItem>
-                </Select>
-            </FormControl>
+  return (
+    <Box display="flex" alignItems="center" justifyContent="center">
+      <TextField
+        select
+        label="Translate from"
+        value={languageFrom}
+        onChange={handleLanguageFromChange}
+        size="small"
+        variant="outlined"
+        style={{ width: 110, margin: 10 }}
+      >
+        <MenuItem value="Text">Text</MenuItem>
+        <MenuItem value="Emojis">Emojis</MenuItem>
+      </TextField>
 
-            <IconButton aria-label="swap" size="large">
-                <SwapHorizIcon onClick={handleSwap}/>
-            </IconButton>
+      <IconButton aria-label="swap" size="large" onClick={handleSwap}>
+        <SwapHorizIcon />
+      </IconButton>
 
-            <FormControl variant="filled" style={{ minWidth: 120, margin: 10 }}>
-                <InputLabel id="select-language-to-label">Translate to</InputLabel>
-                <Select
-                    labelId="select-language-to-label"
-                    id="select-language-to"
-                    value={languageTo}
-                    onChange={handleLanguageToChange}
-                    size="small"
-                >
-                    <MenuItem value="Text">Text</MenuItem>
-                    <MenuItem value="Emojis">Emojis</MenuItem>
-                </Select>
-            </FormControl>
-        </div>
-    );
+      <TextField
+        select
+        label="Translate to"
+        value={languageTo}
+        onChange={handleLanguageToChange}
+        size="small"
+        variant="outlined"
+        style={{ width: 110, margin: 10 }}
+      >
+        <MenuItem value="Text">Text</MenuItem>
+        <MenuItem value="Emojis">Emojis</MenuItem>
+      </TextField>
+    </Box>
+  );
 }
